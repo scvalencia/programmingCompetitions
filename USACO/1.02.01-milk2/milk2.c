8e66cd2e1e9e1a769f8601ab878427a194dc228f
scvalencia <sc.valencia606@uniandes.edu.co>
2015-11-16 10:14:24 -0500

    Keep solving USACO training problems

--- USACO/1.02.01-milk2/milk2.c
+++ USACO/1.02.01-milk2/milk2.c
@@ -1,5 +1,5 @@
 /*
-ID: **.****#
+ID: sc.vale2
 LANG: C
 TASK: milk2
 */
@@ -10,22 +10,12 @@ TASK: milk2
 
 #define SIZE 1000000 + 1
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 void fill(bool* array, int init, int end, bool value) {
     int i = init;
     for(; i <= end; i++)
         *(array + i) = value;
-}
-
-void bounds(bool* array, int* init, int* end) {
-    int i = 0;
-    for(; i <= SIZE && *init == -1; i++)
-        if(*(array + i))
-            *init = i;
-    
-    for(i = SIZE; i >= 0 && *end == -1; i--)
-        if(*(array + i))
-            *end = i;
 }
 
 int main(int argc, char const *argv[]) {
@@ -34,53 +24,42 @@ int main(int argc, char const *argv[]) {
 
     bool *array = malloc(SIZE * sizeof(bool));
     fill(array, 0, SIZE - 1, false);
-    int N, a, b, i = 1, max_true = 0, max_false = 0, count = 0, max_total = 0;
-    int init = -1, end = -1, *p_init, *p_end;
-    p_init = &init;
-    p_end = &end;
-    bool current, value;
+    int N, a, b, i = 1, max_true = 0, max_false = 0, j = 0;
+    int init = 50000 * 2, end = -1, count = 0;
+    bool current, value; 
 
     fscanf(fin, "%d", &N);
 
     while(N--) {
         fscanf(fin, "%d %d", &a, &b);
         fill(array, a, b, true);
+        init = MIN(init, a);
+        end = MAX(end, b);
     }
 
-    bounds(array, p_init, p_end);
+    j = 0;
+    a = init;
 
     current = true;
-    for (i = init; i <= end; i++) {
+
+    for (i = init + 1; i <= end; i++) {
         value = *(array + i);
-        if (current == value)
+        if (current != value) {
+            ++j;
+            if(count % 2 == 0)
+                max_true = MAX(max_true, ((j % 2 == 0) ? i : i - 1) - a);
+            else
+                max_false = MAX(max_false, ((j % 2 == 0) ? i : i - 1) - a);
+            a = ((j % 2 == 0) ? i : i - 1);
+            printf("%d ", i);
             count++;
-        else {
-            if(current && count > max_true)
-                max_true = count;
-            if (!current && count > max_false)
-                max_false = count;
             current = value;
-            count = 1;
         }
     }
-    /*
-    current = *(array + init);
 
-    for(i = init + 1; i <= end; i++) {
-        value = *(array + i);
-        count++;
-        if(current == value) {
-            if(current)
-                max_true = (count > max_true) ? count : max_true;
-            else
-                max_false = (count > max_false) ? count : max_false;
-        } else
-            count = 0; 
-        current = value;
-    }
-    */
+    max_true = MAX(max_true, end - a); 
 
-    printf("%d %d\n", max_true, max_false);
+    fprintf(fout, "%d %d\n", max_true, max_false);
 
     free(array);
     return 0;
