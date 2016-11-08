fce1f1df932e45298f34df7edd0f9e4e56db1317
scvalencia <sc.valencia606@uniandes.edu.co>
2016-11-07 22:24:07 -0500

    Adding a basic solved problem on the Kattis OJ

---
+++ Kattis/fizzbuzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+	int n, x, y, i = 1;
+	scanf("%d %d %d", &x, &y, &n);
+
+	while(i <= n) {
+		if((i % x) == 0 && (i % y) == 0)
+			printf("FizzBuzz\n");
+		else if((i % x) == 0)
+			printf("Fizz\n");
+		else if((i % y) == 0)
+			printf("Buzz\n");
+		else
+			printf("%d\n", i);
+		i++;
+	}
+
+
+
+	return 0;
+}
