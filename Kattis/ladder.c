202ab1f1de5ad7585ef146f48bd1e5f90dec45e1
scvalencia <sc.valencia606@uniandes.edu.co>
2016-11-07 22:23:46 -0500

    Adding a basic solved problem on the Kattis OJ

---
+++ Kattis/ladder.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define PI 3.141592653
+
+int main(int argc, char const *argv[]) {
+
+	int angle, h;
+
+	scanf("%d %d", &h, &angle);
+	
+	float rate = cos(angle * PI / 180);
+	rate /= sin(angle * PI / 180);
+	rate *= h;
+	
+	printf("%d\n", (int) ceil(sqrt(h * h + rate * rate)));
+	
+	return 0;
+}
