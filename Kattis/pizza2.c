4cd706ddbd20918d4bbf25b5325668b49a24da73
scvalencia <sc.valencia606@uniandes.edu.co>
2016-11-07 22:23:23 -0500

    Adding a basic solved problem on the Kattis OJ

---
+++ Kattis/pizza2.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+	int r, c;
+	scanf("%d %d", &r, &c);
+
+	printf("%.7f\n", (c > r) ? 0.0 : pow((10 * (r - c)) / r, 2));
+
+	return 0;
+}
