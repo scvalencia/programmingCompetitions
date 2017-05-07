de218993b485b3835f382c5baf1bafbde1237fba
scvalencia <sc.valencia606@uniandes.edu.co>
2017-05-06 21:45:39 -0500

    Solving basic problems in Kattis

--- Kattis/mixedfractions.c
+++ Kattis/mixedfractions.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char const *argv[]) {
 	int num = 1, den = 1;
+	
 	while(scanf("%d %d", &num, &den) && 
 		(num + den != 0)) {
 			printf("%d %d / %d\n", (num / den), (num % den), den);
